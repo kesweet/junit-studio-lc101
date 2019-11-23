5e6144b502e2060ff820ff5ffc644ebb47dca996
Katherine Sweet <katherine.sweet@slu.edu>
2019-11-23 15:50:28 -0600

    Finished studio, included multiple solution options (not all of them work)

--- src/main/BalancedBrackets.java
+++ src/main/BalancedBrackets.java
@@ -21,6 +21,74 @@ public class BalancedBrackets {
      * @param str - to be validated
      * @return true if balanced, false otherwise
      */
+//    public static boolean hasBalancedBrackets(String str) {
+//        int leftBrack = 0;
+//        int rightBrack = 0;
+//        int l = str.toCharArray().length;
+//        for (char ch : str.toCharArray()) {
+//            if (ch == '[') {
+//                leftBrack++;
+//                int k = 0;
+//                for (int i = 0; i < str.toCharArray().length; i++) {
+//                    if (str.toCharArray()[i] == ch) {
+//                        k = i;
+//                        break;
+//                    }
+//                }
+//                Boolean booleanCondition = false;
+//                for (int j = k; j < l; j++) {
+//                    if (str.toCharArray()[j] == ']') {
+//                        rightBrack++;
+//                        l = j;
+//                        booleanCondition = true;
+//                        break;
+//                    }
+//                }
+//                if (!booleanCondition) {
+//                    return false;
+//                }
+//            }
+//            if (ch == ']') {
+//                rightBrack++;
+//            }
+//        }
+//        return leftBrack == rightBrack;
+//    }
+
+//    public static boolean hasBalancedBrackets(String str) {
+//        int brackets = 0;
+//        for(int i=0; i<str.length(); i++)  {
+//            if (str.charAt(i) == '['&& brackets == 0) {
+//                brackets++;
+//            } else if (str.charAt(i)==']') {
+//                brackets--;
+//            }
+//        }
+//        return brackets == 0;
+//    }
+
+//    public static boolean hasBalancedBrackets(String str) {
+//        int brackets = 0;
+//        char[] someChar = new char [str.length()];
+//        someChar = str.toCharArray();
+//        for (int i = 0; i < str.length(); i++) {
+//            if (someChar[i] == '[') {
+//                if (brackets == 0) {
+//                    brackets++;
+//                } else {
+//                    return false;
+//                }
+//            } else if (someChar[i] == ']') {
+//                if (brackets == 1) {
+//                    brackets--;
+//                } else {
+//                    return false;
+//                }
+//            }
+//        }
+//        return brackets == 0;
+//    }
+
     public static boolean hasBalancedBrackets(String str) {
         int brackets = 0;
         for (char ch : str.toCharArray()) {
@@ -28,6 +96,8 @@ public class BalancedBrackets {
                 brackets++;
             } else if (ch == ']') {
                 brackets--;
+                if(brackets<0)
+                    return false;
             }
         }
         return brackets == 0;
--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,16 +1,100 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
-
+    BalancedBrackets test_Class = new BalancedBrackets();
     //TODO: add tests here
     @Test
     public void emptyTest() {
         assertEquals(true, true);
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(test_Class.hasBalancedBrackets("[]"));
+    }
 
+    @Test
+    public void emptyStringReturnsTrue() {
+        assertTrue(test_Class.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void rightBracketOnlyReturnsFalse() {
+        assertFalse(test_Class.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void leftBracketOnlyReturnsFalse() {
+        assertFalse(test_Class.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void rightBracketWithCharReturnsFalse() {
+        assertFalse(test_Class.hasBalancedBrackets("]wrong"));
+    }
+
+    @Test
+    public void rightBracketWithLeftAfterReturnsFalse() {
+        assertFalse(test_Class.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void rightBracketWithCharAndLeftReturnsFalse() {
+        assertFalse(test_Class.hasBalancedBrackets("]wrong["));
+    }
+
+    @Test
+    public void twoSetsOfBracketsWithRightFirstReturnsFalse() {
+        assertFalse(test_Class.hasBalancedBrackets("][]["));
+    }
+
+    @Test
+    public void rightBracketTwiceReturnsFalse() {
+        assertFalse(test_Class.hasBalancedBrackets("]]"));
+    }
+
+    @Test
+    public void rightBracketTwiceWithLeftReturnsFalse() {
+        assertFalse(test_Class.hasBalancedBrackets("]][]"));
+    }
+
+    @Test
+    public void twoLeftBracketsReturnsFalse() {
+        assertFalse(test_Class.hasBalancedBrackets("[["));
+    }
+
+    @Test
+    public void leftLeftRightBracketsReturnsFalse() {
+        assertFalse(test_Class.hasBalancedBrackets("[[]"));
+    }
+
+    @Test
+    public void bracketsWithStringReturnsTrue() {
+        assertTrue(test_Class.hasBalancedBrackets("[string]"));
+    }
+
+    @Test
+    public void bracketsBeforeStringReturnsTrue() {
+        assertTrue(test_Class.hasBalancedBrackets("[]string"));
+    }
+
+    @Test
+    public void bracketsAfterStringReturnsTrue() {
+        assertTrue(test_Class.hasBalancedBrackets("string[]"));
+    }
+
+    @Test
+    public void bracketsMakingSubstringReturnsTrue() {
+        assertTrue(test_Class.hasBalancedBrackets("st[ring]"));
+    }
+
+    @Test
+    public void embeddedBracketsReturnsTrue() {
+        assertTrue(test_Class.hasBalancedBrackets("[[]][ring]"));
+    }
 }
